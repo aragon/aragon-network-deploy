1c4128e96b63c8c2fa320be70f417140f2d21500
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2019-11-22 09:34:11 -0300

    Mint: Implement mint command (#8)

--- src/commands/court.js
+++ src/commands/court.js
@@ -10,6 +10,7 @@ const describe = 'Deploy Court core contracts'
 const builder = {
   output: { alias: 'o', describe: 'Output dir', type: 'string', default: './data/output' },
   config: { alias: 'c', describe: 'Court config JSON file', type: 'string', default: './data/config/court.js' },
+  verify: { describe: 'Verify deployed contracts on Etherscan, provide API key', type: 'string' },
 }
 
 const handlerAsync = async (environment, { network, verify: apiKey, output: outputDir, config: configFilename }) => {
