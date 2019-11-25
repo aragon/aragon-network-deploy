5921eff4b440d580bccbfd3cdce5e51f787d9ce6
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2019-11-25 16:49:18 -0300

    Court: Use MCD as fee tokens for mainnet

--- data/config/court.mainnet.js
+++ data/config/court.mainnet.js
@@ -12,7 +12,7 @@ const ANJ = {
 const DAI = {
   symbol: 'DAI',
   decimals: 18,
-  address: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359'
+  address: '0x6b175474e89094c44da98b954eedeac495271d0f'          // MCD
 }
 
 module.exports = {
