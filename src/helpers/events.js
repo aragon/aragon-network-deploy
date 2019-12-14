f60b6818d88a7d9aa6e760b9ffa0bd9eb5ff48fe
ßingen <bingen@aragon.one>
2019-12-14 19:07:00 +0100

    Presale: add command option to get fundrasing apps from receipt

--- src/helpers/events.js
+++ src/helpers/events.js
@@ -1,9 +1,9 @@
 const abi = require('web3-eth-abi')
 
-const decodeEvents = ({ receipt }, contractAbi, eventName) => {
+const decodeEvents = (logs, contractAbi, eventName) => {
   const eventAbi = contractAbi.filter(abi => abi.name === eventName && abi.type === 'event')[0]
   const eventSignature = abi.encodeEventSignature(eventAbi)
-  const eventLogs = receipt.rawLogs.filter(l => l.topics[0] === eventSignature)
+  const eventLogs = logs.filter(l => l.topics[0] === eventSignature)
   return eventLogs.map(log => {
     log.event = eventAbi.name
     log.args = abi.decodeLog(eventAbi.inputs, log.data, log.topics.slice(1))
@@ -11,16 +11,16 @@ const decodeEvents = ({ receipt }, contractAbi, eventName) => {
   })
 }
 
-const getInstalledApps = (receipt, appId, kernel) => {
-  const events = decodeEvents(receipt, kernel.abi, 'NewAppProxy')
+const getInstalledApps = (logs, appId, kernel) => {
+  const events = decodeEvents(logs, kernel.abi, 'NewAppProxy')
   const appEvents = events.filter(e => e.args.appId === appId)
   const installedAddresses = appEvents.map(e => e.args.proxy)
   return installedAddresses
 }
 
-const getInstalledAppsById = (receipt, appIds, kernel) => {
+const getInstalledAppsById = (logs, appIds, kernel) => {
   return Object.keys(appIds).reduce((apps, appName) => {
-    apps[appName] = getInstalledApps(receipt, appIds[appName], kernel)
+    apps[appName] = getInstalledApps(logs, appIds[appName], kernel)
     return apps
   }, {})
 }
