6925a5234bd8cebe3acbaebeec59fa1674d91199
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2019-12-16 12:45:38 -0300

    presale: fix rpc config file

--- data/config/presale.rpc.js
+++ data/config/presale.rpc.js
@@ -1,29 +1,30 @@
+const START_DATE = Math.floor(new Date() / 1000 + 600).toString()         // 10 minutes from now
+
 module.exports = {
   infra: {
-    ens: '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1',        // aragen ENS
-    daoFactory: '0x5d94e3e7aec542ab0f9129b9a7badeb5b3ca0f77', // aragen DAO factory
-    registrar: '0xf1f8aac64036cdd399886b1c157b7e3b361093f3'   // aragen aragonID
+    ens:              '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1',       // Aragen ENS instance
+    daoFactory:       '0x5d94e3e7aec542ab0f9129b9a7badeb5b3ca0f77',       // DAO factory used for the templates in Aragen
+    registrar:        '0xf1f8aac64036cdd399886b1c157b7e3b361093f3'        // Aragen aragonID
   },
   instance: {
-    owner: '0xb4124cEB3451635DAcedd11767f004d8a28c6eE7',      // aragen address #1
-    id: 'anjpresale',
-    collateralToken: '0x2e25c8F88c5cCcbC9400e5bc86cF9C58C7604327',
-    bondedToken: '0x019F12a7DAa44A34CD0FF35055b0e2D4679D8521',
-    period: '600',                                            // 10 min
-    openDate: '0',                                            // manual presale start
-    exchangeRate: '100000000',                                // 1/100
-    beneficiaryPct: '200000',                                 // 20%, pre-minting
-    reserveRatio: '33333',                                    // 33.333%
-    batchBlocks: '1',
-    slippage: '100000000000000000'                            // 10%
+    owner:            undefined,                                          // Set with an AN DAO deployed to Aragen
+    id:               'templates-externally-owned-presale-bonding-curve', // ID of the template to be registered in aragon ID
+    collateralToken:  undefined,                                          // Set with the ANT instance deployed to Aragen
+    bondedToken:      undefined,                                          // Set with the ANJ instance deployed to Aragen
+    period:           '600',                                              // Presale period will last 10 minutes, bonding curve will start right after that
+    exchangeRate:     '100000000',                                        // Presale exchange rate 1 ANT = 100 ANJ
+    openDate:         START_DATE,                                         // Presale will start in 10 minutes from now
+    beneficiaryPct:   '200000',                                           // 20% of the generated ANJ will be minted for the owner
+    reserveRatio:     '33333',                                            // Bonding curve reserve ratio of 33%, also used to compute the virtual balance
+    batchBlocks:      '1',                                                // Market maker batch size
+    slippage:         '100000000000000000'                                // 10%
   },
-  aragonNetworkDao: {
-    votingApp: '0x3bf37efD74354A29f9507D1A21329b773bB49F58',
-    tokenManager: '0xEB62b78213c6C2E5c0698dDa67E3260007b8C999',
-    voteDescription: 'Change ANJ controller to template and deploy new presale and bonding curve instance'
+  aragonNetworkDAO: {
+    votingApp:        undefined,                                          // Set with the voting app of the AN DAO deployed to Aragen
+    tokenManager:     undefined,                                          // Set with the token manager of the AN DAO deployed to Aragen
   },
   wrapper: {
-    registry: '0x4eD6178D5dEf6AFD8e23334038609125cBB15C8F',   // Court Jurors Registry
-    presale: '0x1902a0410EFe699487Dd85F12321aD672bE4ada2'     // Fundraising presale contract
+    registry:         '0x4eD6178D5dEf6AFD8e23334038609125cBB15C8F',       // Court's Jurors Registry instance on Aragen
+    presale:          '0x1902a0410EFe699487Dd85F12321aD672bE4ada2'        // Fundraising presale instance on Aragen
   }
 }
