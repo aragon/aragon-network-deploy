013a736d436017b69cf9aaae7cdc72422dcd5af0
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-02-07 13:51:59 -0300

    prepare mainnet migration

--- data/config/registry-migration.mainnet.js
+++ data/config/registry-migration.mainnet.js
@@ -1,8 +1,7 @@
 const { bn } = require('../../src/helpers/numbers')
 
-const TERM_DURATION = 60 * 10                                        // 10 minutes
-// 2020, Feb 10th 20.00 UTC, three years after: https://twitter.com/AragonProject/status/830013470737309697
-const START_DATE = Math.floor(new Date('2020-02-10T20:00:00.000Z') / 1000)
+// 2020, Feb 10th 16.00 UTC, three years after: https://twitter.com/AragonProject/status/830013470737309697
+const START_DATE = Math.floor(new Date('2020-02-10T16:00:00.000Z') / 1000)
 
 module.exports = {
   owner:              '0x5e8c17a6065c35b172b10e80493d2266e2947df4',       // Agent of AN DAO
@@ -14,7 +13,7 @@ module.exports = {
   court:              '0xee4650cBe7a2B23701D416f58b41D8B76b617797',       // Court's controller
   disputeManager:     '0xCb36d7614ED8283cBb7e606AF876c75b372bB859',       // Original Dispute Manager
   oldRegistry:        '0xF9dDa954adf5E54b89f988C1560553A0a387CCE1',       // Court's old Jurors Registry instance
-  newRegistry:        '',       // Court's new Jurors Registry instance
-  migrator:           '',       // Address of migrator contract
-  firstTermStartTime:  bn(START_DATE),                                    // first term start timestamp in seconds
+  newRegistry:        '0x0F7471C1df2021fF45f112878F755aAbe7AA16bF',       // Court's new Jurors Registry instance
+  migrator:           '0x68380629555108aEae92e5a4C92973943073E811',       // Address of the migrator contract
+  firstTermStartTime:  bn(START_DATE),                                    // First term start timestamp in seconds
 }
