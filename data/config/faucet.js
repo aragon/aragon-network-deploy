526f1a0cb20486f913db6862899b7f2a6a52b7ad
ßingen <bingen@aragon.one>
2020-02-11 20:38:13 +0100

    Add Rinkeby usability testing addresses (#44)
    * Update usability test in Rinkeby
    
    * Add Rinkeby config file for config change through CLI
    
    * move usability testing configs to a separate file
    
    Co-authored-by: Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>

--- data/config/faucet.js
+++ data/config/faucet.js
@@ -53,7 +53,30 @@ module.exports = {
         symbol:   'DAI',
         address:  '0xe9A083D88Eed757B1d633321Ce0519F432c6284d',       // Fee token (DAI) address in Rinkeby
         period:   ONE_WEEK,                                           // The fee token quota period lasts 1 week
-        amount:   bigExp(10000, 18)                                   // Accounts will be allowed to withdraw 1000 ANT per week maximum
+        amount:   bigExp(10000, 18)                                   // Accounts will be allowed to withdraw 10000 DAI per week maximum
+      }
+    ]
+  },
+  usability: {
+    owner:        undefined,                                          // Ownership will remain to the sender
+    tokens: [
+      {
+        symbol:   'ANT',
+        address:  '0x5cC7986D7A793b9930BD80067ca54c3E6D2F261B',       // ANT address in Rinkeby for usability testing
+        period:   ONE_WEEK,                                           // The ANT quota period lasts 1 week
+        amount:   bigExp(1000, 18)                                    // Accounts will be allowed to withdraw 1000 DAI per week maximum
+      },
+      {
+        symbol:   'ANJ',
+        address:  '0x929F3B27a22a7A56FC8d89617033D22e53840aC9',       // ANT address in Rinkeby for usability testing
+        period:   ONE_WEEK,                                           // The ANT quota period lasts 1 week
+        amount:   bigExp(10000, 18)                                   // Accounts will be allowed to withdraw 10000 ANJ per week maximum
+      },
+      {
+        symbol:   'DAI',
+        address:  '0x55Ab9B236CDC9e2CecBD41ADa45D8261f8A6049b',       // Fee token (DAI) address in Rinkeby for usability testing
+        period:   ONE_WEEK,                                           // The fee token quota period lasts 1 week
+        amount:   bigExp(10000, 18)                                   // Accounts will be allowed to withdraw 10000 ANT per week maximum
       }
     ]
   },
---
+++ data/config/presale.rinkeby.usability.js
@@ -0,0 +1,32 @@
+const START_DATE = Math.floor(new Date() / 1000 + 600).toString()         // 10 minutes from now
+
+module.exports = {
+  infra: {
+    ens:              '0x98df287b6c145399aaa709692c8d308357bc085d',       // Rinkeby ENS instance
+    daoFactory:       '0xad4d106b43b480faa3ef7f98464ffc27fc1faa96',       // DAO factory used for the templates in Rinkeby
+    registrar:        '0x3665e7bfd4d3254ae7796779800f5b603c43c60d'        // Rinkeby aragonID
+  },
+  instance: {
+    owner:            '0x9aedfbe0f3ed33cbb71f937cb9eb0ff1f4dfc076',       // Agent of AN DAO Rinkeby
+    id:               'uw-001',                                           // ID of the template to be registered in aragon ID
+    collateralToken:  '0x2D23F49a6C1B60e614952d8b0D53bd7a2DbF6433',       // ANT
+    bondedToken:      '0x929F3B27a22a7A56FC8d89617033D22e53840aC9',       // ANJ
+    period:           '864000',                                           // Presale period will last 10 days, bonding curve will start right after that
+    exchangeRate:     '100000000',                                        // Presale exchange rate 1 ANT = 100 ANJ
+    openDate:         START_DATE,                                         // Presale will start in 10 minutes from now
+    beneficiaryPct:   '200000',                                           // 20% of the generated ANJ will be minted for the owner
+    reserveRatio:     '33333',                                            // Bonding curve reserve ratio of 33%, also used to compute the virtual balance
+    batchBlocks:      '1',                                                // Market maker batch size
+    slippage:         '100000000000000000'                                // 10%
+  },
+  aragonNetworkDAO: {
+    votingApp:        '0xb0c839957bc47541d747e5238c06990e3a41c2a3',       // Voting of AN DAO Rinkeby
+    tokenManager:     '0x0cc31e2d5117d25fe50ee9d569613851e6bb0dd9',       // Token Manager of AN DAO Rinkeby
+  },
+  wrapper: {
+    owner:            '0x9aedfbe0f3ed33cbb71f937cb9eb0ff1f4dfc076',       // Agent of AN DAO Rinkeby
+    registry:         '0x8529A0aF5b39C7Ab5Df6E20Aa3bf44B0F82fF680',       // Court's Jurors Registry instance on Rinkeby
+    presale:          '0x51974a685c653aFECfB383017eAb65A501f35aD9',       // Fundraising presale instance on Rinkeby
+    uniswap:          '0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36',       // Uniswap Factory address on Rinkeby
+  }
+}
