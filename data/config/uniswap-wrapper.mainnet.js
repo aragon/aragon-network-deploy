b2bfb7be2c0eb8cad79e7d6ad056b03ad5daf917
ßingen <bingen@aragon.one>
2020-02-12 01:20:47 +0100

    Uniswap wrapper: fix mainnet registry
    Switch old by new.

--- data/config/uniswap-wrapper.mainnet.js
+++ data/config/uniswap-wrapper.mainnet.js
@@ -1,6 +1,6 @@
 module.exports = {
   owner:            '0x5e8c17a6065c35b172b10e80493d2266e2947df4',       // Agent of AN DAO
   bondedToken:      '0xcD62b1C403fa761BAadFC74C525ce2B51780b184',       // ANJ
-  registry:         '0xF9dDa954adf5E54b89f988C1560553A0a387CCE1',       // Court's Jurors Registry instance on Mainnet
+  registry:         '0x0F7471C1df2021fF45f112878F755aAbe7AA16bF',       // Court's Jurors Registry instance on Mainnet
   uniswap:          '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95',       // Uniswap Factory address
 }
