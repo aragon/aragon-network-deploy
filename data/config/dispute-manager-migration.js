e65b996663d55838ac5f554abab3b337111414a9
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-03-17 17:26:09 -0300

    usability: adapt commands for usability instance

--- data/config/dispute-manager-migration.js
+++ data/config/dispute-manager-migration.js
@@ -1,11 +1,13 @@
 const rpc = require('./dispute-manager-migration.rpc')
 const staging = require('./dispute-manager-migration.staging')
+const usability = require('./dispute-manager-migration.usability')
 const rinkeby = require('./dispute-manager-migration.rinkeby')
 const mainnet = require('./dispute-manager-migration.mainnet')
 
 const environments = {
   rpc,
   staging,
+  usability,
   rinkeby,
   mainnet
 }
--- data/config/presale.js
+++ data/config/presale.js
@@ -2,6 +2,7 @@ const rpc = require('./presale.rpc')
 const staging = require('./presale.staging')
 const rinkeby = require('./presale.rinkeby')
 const mainnet = require('./presale.mainnet')
+const usability = require('./presale.usability')
 
 // There is no APM registry on Ropsten, thus we cannot publish the pre-sale there to use it from a DAO
 
@@ -9,5 +10,6 @@ module.exports = {
   rpc,
   staging,
   rinkeby,
-  mainnet
+  mainnet,
+  usability
 }
