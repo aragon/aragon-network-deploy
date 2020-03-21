a520e30648f72dd805e812c5392a91a3a2c28d32
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-03-21 03:11:28 -0300

    migrator: extend to support closing

--- src/commands/migrate-registry.js
+++ src/commands/migrate-registry.js
@@ -1,23 +1,20 @@
 const path = require('path')
 const JurorsRegistryMigrator = require('../models/deployers/JurorsRegistryMigrator')
 
-const command = 'registry-migrate'
+const command = 'migrate-registry'
 const describe = 'Migrate JurorsRegistry'
 
 const builder = {
+  close: { describe: 'Whether to set up a new migrator or request closing an existing one', type: 'boolean', default: false },
   output: { alias: 'o', describe: 'Output dir', type: 'string', default: './data/output' },
-  config: { alias: 'c', describe: 'Migration config JSON file', type: 'string', default: './data/config/registry-migration.js' },
+  config: { alias: 'c', describe: 'Migration config JS file', type: 'string', default: './data/config/registry-migration.js' },
 }
 
-const handlerAsync = async (environment, {
-  network,
-  output: outputDir,
-  config: configFilename,
-}) => {
+const handlerAsync = async (environment, { close, network, output: outputDir, config: configFilename }) => {
   const outputFilepath = path.resolve(process.cwd(), `${outputDir}/${command}.${network}.json`)
   const config = require(path.resolve(process.cwd(), configFilename))[network]
 
-  const deployer = new JurorsRegistryMigrator(config, environment, outputFilepath)
+  const deployer = new JurorsRegistryMigrator(config, environment, outputFilepath, close)
   await deployer.call()
 }
 
--- src/models/deployers/JurorsRegistryMigrator.js
+++ src/models/deployers/JurorsRegistryMigrator.js
@@ -4,69 +4,74 @@ const logger = require('../../helpers/logger')('JurorsRegistryMigrator')
 const { JURORS_REGISTRY_ID, DISPUTE_MANAGER_ID } = require('../../helpers/court-modules')
 
 module.exports = class extends BaseDeployer {
-  constructor(
-    config,
-    environment,
-    output,
-    verifier = undefined,
-  ) {
-    super(environment, output, verifier, logger)
+  constructor(config, environment, output, close = false) {
+    super(environment, output, undefined, logger)
     this.config = config
+    this.close = close
     this.encoder = new CallsEncoder()
   }
 
   async call() {
-    const callsScript = this._generateEvmScript()
-    logger.success(`Call script for AN DAO token manager generated`)
-    logger.info(`${callsScript}`)
-    const { tokenManager } = this.config.aragonNetworkDAO
-    const receipt = await this._runEvmScript(callsScript, tokenManager)
-    logger.success(`EVM Script run on tx: ${receipt.tx}. Gas used: ${receipt.receipt.gasUsed}`)
+    if (!this.close) await this._setupMigrator()
+    else await this._closeMigrator()
   }
 
-  _generateEvmScript() {
-    const {
-      owner,
-      bondedToken,
-      court,
-      oldRegistry,
-      newRegistry,
-      migrator,
-    } = this.config
-
-    const agentCallsScript = []
-    agentCallsScript.push({
-      to: owner,
-      data: this._recoverFundsFromOldRegistryScript(oldRegistry, bondedToken, migrator)
-    })
-    agentCallsScript.push({
-      to: owner,
-      data: this._setModulesScript(court, JURORS_REGISTRY_ID, newRegistry, DISPUTE_MANAGER_ID, migrator)
-    })
-
-    const { voting } = this.config.aragonNetworkDAO
-    const voteDescription = ''
-    const tokenManagerScript = [{
-      to: voting,
-      data: this.encoder.encodeNewVote(this.encoder.encodeCallsScript(agentCallsScript), voteDescription)
-    }]
-    return this.encoder.encodeCallsScript(tokenManagerScript)
+  async _setupMigrator() {
+    if (typeof this.config.governor === 'string') await this._setupMigratorDirectly()
+    else await this._setupMigratorThroughDAO()
   }
 
-  _recoverFundsFromOldRegistryScript(oldJurorsRegistryAddress, anjAddress, anDaoAgent) {
-    const data = this.encoder.encodeRecoverFunds(anjAddress, anDaoAgent)
+  async _setupMigratorDirectly() {
+    const { bondedToken, court, oldRegistry, newRegistry, migrator } = this.config
+
+    logger.info('Sending funds from old registry to migrator...')
+    const JurorsRegistry = await this.environment.getArtifact('JurorsRegistry', '@aragon/court')
+    const oldJurorsRegistry = await JurorsRegistry.at(oldRegistry)
+    await oldJurorsRegistry.recoverFunds(bondedToken, migrator)
+    logger.success('Funds successfully transferred to migrator')
+
+    logger.info('Setting modules...')
+    const AragonCourt = await this.environment.getArtifact('AragonCourt', '@aragon/court')
+    const controller = await AragonCourt.at(court)
+    await controller.setModules([JURORS_REGISTRY_ID, DISPUTE_MANAGER_ID], [newRegistry, migrator])
+    logger.success('Modules set successfully')
+    logger.success('Migrator set up successfully')
+  }
+
+  async _setupMigratorThroughDAO() {
+    const { governor, bondedToken, court, oldRegistry, newRegistry, migrator } = this.config
+    logger.info('Building EVM script to set up registry migrator...')
+    const recoverFundsData = this._buildRecoverFundsData(oldRegistry, bondedToken, migrator)
+    const setModulesData = this._buildSetModulesData(court, newRegistry, migrator)
+    const agentCallsScript = [{ to: governor.agent, data: recoverFundsData }, { to: governor.agent, data: setModulesData }]
+    await this._encodeAndSubmitEvmScript(governor, agentCallsScript, 'Set up registry migrator')
+  }
+
+  _buildRecoverFundsData(oldJurorsRegistryAddress, anj, migrator) {
+    const data = this.encoder.encodeRecoverFunds(anj, migrator)
     return this.encoder.encodeExecute(oldJurorsRegistryAddress, 0, data)
   }
 
-  // sets the new registry and the migrator as dispute manager
-  _setModulesScript(
-    controllerAddress,
-    jurorsRegistryId,
-    newJurorsRegistryAddress,
-    disputeManagerId,
-    migratorAddress
-  ) {
-    const data = this.encoder.encodeSetModules([jurorsRegistryId, disputeManagerId], [newJurorsRegistryAddress, migratorAddress])
-    return this.encoder.encodeExecute(controllerAddress, 0, data)
+  _buildSetModulesData(controller, newJurorsRegistry, migrator) {
+    const data = this.encoder.encodeSetModules([JURORS_REGISTRY_ID, DISPUTE_MANAGER_ID], [newJurorsRegistry, migrator])
+    return this.encoder.encodeExecute(controller, 0, data)
+  }
+
+  async _closeMigrator() {
+    const { governor, migrator } = this.config
+
+    if (typeof governor === 'string') {
+      logger.info('Closing migrator...')
+      const JurorsRegistryMigrator = await this.environment.getArtifact('JurorsRegistryMigrator', '@aragonone/court-registry-migrator')
+      const registryMigrator = await JurorsRegistryMigrator.at(migrator)
+      await registryMigrator.close()
+      logger.success('Migrator closed successfully')
+    } else {
+      logger.info('Building EVM script to close migrator...')
+      const closeData = this.encoder.encodeCloseMigrator()
+      const executeData = this.encoder.encodeExecute(migrator, 0, closeData)
+      const agentCallsScript = [{ to: governor.agent, data: executeData }]
+      await this._encodeAndSubmitEvmScript(governor, agentCallsScript, 'Close registry migrator')
+    }
   }
 }
